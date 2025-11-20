f4bcb5aec1341a271a6c4786f0bbe6fb85426a59
Gautam Awasthi <gaawasthi@bestpeers.in>
2025-11-20 18:12:21 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://ecommercebestpeers-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       },
